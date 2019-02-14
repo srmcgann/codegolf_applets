7f168748cbb88981cc6f12ce68b73f83c28bd4cc
Scott McGann <s.r.mcgann@hotmail.com>
2019-02-13 16:00:39 -0800

    initial commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?
+	$db_user="user";
+	$db_pass="";
+	$db_host="localhost";
+	$db="codegolf";
+	$baseDomain="codegolf.tk";
+	$appletDomain="applet.codegolf.tk";
+        $legacyDomain="legacy.codegolf.tk";
+	$baseURL="https://$baseDomain";
+        $legacyURL="https://$legacyDomain";
+	$appletURL="https://$appletDomain";
+	$link = mysqli_connect($db_host, $db_user, $db_pass, $db);
+?>
